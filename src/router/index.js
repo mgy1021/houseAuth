1e5687e2cce8f77618884352976130ff85474222
ljyuan <2388469764@qq,com>
2021-10-26 09:48:49 +0800

    'ljy'

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@
  * @Description: 
  * @Author: Mogy
  * @Date: 2021-10-13 15:44:13
- * @LastEditors: Mogy
- * @LastEditTime: 2021-10-22 10:39:22
+ * @LastEditors: ljy
+ * @LastEditTime: 2021-10-22 15:38:11
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
@@ -11,41 +11,51 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/news',
-    name: 'News',
-    component: () => import('../views/News.vue')
-  }, {
-    path: '/houseUpdate',
-    name: 'HouseUpdate',
-    component: () => import('../views/HouseUpdate.vue')
-  }, {
-    path: '/queryHouse',
-    name: 'QueryHouse',
-    component: () => import('../views/queryHouse.vue')
-  },
-  {
-    path: '/house',
-    name: 'House',
-    component: () => import('../views/house.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/news',
+        name: 'News',
+        component: () =>
+            import ('../views/News.vue')
+    }, {
+        path: '/houseUpdate',
+        name: 'HouseUpdate',
+        component: () =>
+            import ('../views/HouseUpdate.vue')
+    }, {
+        path: '/queryHouse',
+        name: 'QueryHouse',
+        component: () =>
+            import ('../views/queryHouse.vue')
+    },
+    {
+        path: '/house',
+        name: 'House',
+        component: () =>
+            import ('../views/house.vue')
+    },
+    {
+        path: '/newDetails',
+        name: 'NewDetails',
+        component: () =>
+            import ('../views/newDetails.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'hash',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
--- src/views/News.vue
+++ src/views/News.vue
@@ -1,5 +1,24 @@
+<!--
+ * @Description: 
+ * @Author: ljy
+ * @Date: 2021-10-22 11:29:02
+ * @LastEditors: ljy
+ * @LastEditTime: 2021-10-22 16:12:37
+-->
 <template>
-  <div>新闻中心</div>
+  <div class="news">
+    <div class="head">新闻资讯</div>
+    <div class="new">
+      <div class="title">苹果又遭专利诉讼：因Apple TV快速回放功能侵权</div>
+      <div class="content">
+        <router-link to="/newDetails">
+        一家位于佛罗里达州的数字视频公司正在就苹果公司第四代Apple TV的Siri功能提起专利诉讼。这家名为CustomPlay的公司称，是他们早开发了Apple TV和Siri正在使用的这一功能，该功能允许用户可以询问“他说了什么?”以回顾一小段视频。
+        </router-link>
+      </div>
+      <div class="date">2021-9-9</div>
+      <div class="line"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,4 +33,45 @@ export default {
 };
 </script>
 <style scoped>
+.news{
+  width: 1100px;
+  height: 500px;
+  margin: 50px auto;
+}
+.head{
+  font-size: 28px;
+  margin-bottom: 100px;
+}
+.new{
+  position: relative;
+}
+.title,.content{
+  padding: 5px;
+  text-align: left;
+  margin-bottom: 10px;
+}
+.title{
+  font-size: 20px;
+}
+.content{
+    text-overflow: ellipsis; 
+    overflow: hidden;
+    width: 700px;
+    height: 16px;
+    font-size: 16px;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    margin-bottom: 20px;
+
+}
+.date{
+  position: absolute;
+  right: 30px;
+  top: 10px;
+}
+.line{
+  height: 1px;
+  background-color: #ccc;
+}
 </style>
