e7fe0309a9040768ca316619b5fec54c7aee9ad1
ljyuan <2388469764@qq,com>
2021-10-27 14:57:06 +0800

    '---'

--- src/views/News.vue
+++ src/views/News.vue
@@ -3,8 +3,8 @@
 <<<<<<< HEAD
  * @Author: Mogy
  * @Date: 2021-10-15 13:43:01
- * @LastEditors: Mogy
- * @LastEditTime: 2021-10-25 18:10:29
+ * @LastEditors: ljy
+ * @LastEditTime: 2021-10-27 14:35:09
 =======
  * @Author: ljy
  * @Date: 2021-10-22 11:29:02
@@ -14,17 +14,37 @@
 -->
 <template>
   <div class="news">
-    <div class="head">新闻资讯</div>
+    <div class="header">
+      <div class="line1"></div>
+      <div class="head">新闻资讯</div>
+      <div class="line2"></div>
+      <div class="english">NEWS</div>
+    </div>
     <div class="new">
+      <div class="photo"><img src="../assets/news_1.jpg" alt="" /></div>
+      <router-link to="/newDetails">
+        <div class="title">苹果又遭专利诉讼：因Apple TV快速回放功能侵权</div>
+      </router-link>
+      <div class="date">2021-9-9</div>
+      <div class="content">
+        一家位于佛罗里达州的数字视频公司正在就苹果公司第四代Apple
+        TV的Siri功能提起专利诉讼。这家名为CustomPlay的公司称，是他们早开发了Apple
+        TV和Siri正在使用的这一功能，该功能允许用户可以询问“他说了什么?”以回顾一小段视频。
+      </div>
+      <div class="inline"></div>
+    </div>
+    <!-- <div class="new">
       <div class="title">苹果又遭专利诉讼：因Apple TV快速回放功能侵权</div>
       <div class="content">
         <router-link to="/newDetails">
-        一家位于佛罗里达州的数字视频公司正在就苹果公司第四代Apple TV的Siri功能提起专利诉讼。这家名为CustomPlay的公司称，是他们早开发了Apple TV和Siri正在使用的这一功能，该功能允许用户可以询问“他说了什么?”以回顾一小段视频。
+          一家位于佛罗里达州的数字视频公司正在就苹果公司第四代Apple
+          TV的Siri功能提起专利诉讼。这家名为CustomPlay的公司称，是他们早开发了Apple
+          TV和Siri正在使用的这一功能，该功能允许用户可以询问“他说了什么?”以回顾一小段视频。
         </router-link>
       </div>
       <div class="date">2021-9-9</div>
-      <div class="line"></div>
-    </div>
+      <div class="inline"></div>
+    </div> -->
   </div>
 </template>
 
@@ -48,45 +68,91 @@ export default {
 };
 </script>
 <style scoped>
-.news{
+.news {
   width: 1100px;
   height: 500px;
   margin: 50px auto;
 }
-.head{
-  font-size: 28px;
-  margin-bottom: 100px;
-}
-.new{
+.header {
+  display: flex;
   position: relative;
 }
-.title,.content{
-  padding: 5px;
-  text-align: left;
-  margin-bottom: 10px;
-}
-.title{
-  font-size: 20px;
-}
-.content{
-    text-overflow: ellipsis; 
-    overflow: hidden;
-    width: 700px;
-    height: 16px;
-    font-size: 16px;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 1;
-    margin-bottom: 20px;
-
-}
-.date{
+.line1,
+.line2 {
   position: absolute;
-  right: 30px;
-  top: 10px;
-}
-.line{
+  width: 100px;
   height: 1px;
   background-color: #ccc;
+}
+.line1 {
+  left: 28%;
+  top: 37%;
+}
+.line2 {
+  right: 28%;
+  top: 37%;
+}
+.head {
+  font-size: 32px;
+  margin: 50px auto 100px;
+  border: 1px solid #ccc;
+  width: 300px;
+  line-height: 50px;
+}
+.english {
+  margin-top: 10px;
+  font-size: 26px;
+  color: #ccc;
+  position: absolute;
+  left: 46.5%;
+  top: 50%;
+}
+.new {
+  position: relative;
+  width: 600px;
+  height: 150px;
+  /* background-color: aqua; */
+  /* display: flex; */
+}
+.photo {
+  text-align: left;
+}
+.photo img {
+  width: 200px;
+  height: 150px;
+}
+.title,
+.date,
+.content {
+  position: absolute;
+}
+.title {
+  width: 280px;
+  top: 5px;
+  left: 210px;
+  font-size: 18px;
+}
+.date {
+  top: 5px;
+  right: 10px;
+  font-size: 17px;
+}
+.content {
+  font-size: 16px;
+  margin-top: 10px;
+  top: 60px;
+  left: 210px;
+  width: 380px;
+  /* text-overflow: ellipsis; */
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+}
+.inline {
+  width: 600px;
+  height: 1px;
+  background-color: #ccc;
+  margin-top: 20px;
 }
 </style>
