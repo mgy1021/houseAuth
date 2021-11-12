85887782a65adf16a16c4d5c85262131e62202e7
mgy0034 <8077150+mgy0034@user.noreply.gitee.com>
2021-11-12 11:10:04 +0800

    first commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,13 +2,13 @@
  * @Description:项目开发的配置文件
  * @Author: Mogy
  * @Date: 2021-06-29 15:03:00
- * @LastEditors: ljy
- * @LastEditTime: 2021-10-27 11:16:52
+ * @LastEditors: Mogy
+ * @LastEditTime: 2021-11-02 17:27:49
  */
 
 // axios请求的基路径
-const baseURL = 'http://8.133.167.153:8089'
-    // cookie里token的key值
+const baseURL = 'http://192.168.2.81:3000'
+// cookie里token的key值
 const tokenKey = 'token'
 
 export {
